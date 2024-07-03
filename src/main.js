3e1c381072bfd3b798c421ab1cab95ecf179bf1b
kryvosheyin <alex.kryvosheyin@gmail.com>
2024-07-03 20:31:00 +0100

    added basic js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,42 @@
-// import './js/...';
+import * as api from './js/api.js';
+
+const init = async () => {
+  const categories = await api.getExercisesByCategory();
+  return categories;
+};
+
+const exerciseCategoryList = document.querySelector('.exercises_nav');
+const search = document.querySelector('.exercises_search');
+
+init()
+  .then(resp => console.log(resp))
+  .catch(error => console.log(error));
+
+exerciseCategoryList.addEventListener('click', onCategoryClick);
+
+async function onCategoryClick(event) {
+  try {
+    let target = event.target;
+    let contentTarget = target.textContent.trim();
+    console.log(contentTarget);
+    let curentCategory = document.querySelector('.exercises__nav-item-current');
+
+    const toogleClass = (curentCategory, target) => {
+      curentCategory.classList.remove('exercises__nav-item-current');
+      target.classList.add('exercises__nav-item-current');
+      search.style = 'display: block';
+    };
+
+    //undefined will be handled shortly
+    const res = await api.getExercisesByCategory(
+      undefined,
+      undefined,
+      contentTarget
+    );
+    console.log(res);
+
+    toogleClass(curentCategory, target);
+  } catch (error) {
+    displayError(error.message);
+  }
+}
