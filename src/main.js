20f42bab0356d0d59129161aabdb83474d3d9bad
Darina Khalina <darina.khalina@namecheap.com>
2024-07-04 20:31:12 +0300

    clean up

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { getQuote } from './js/daily-quote.js';
 import { getCategories } from './js/api-service/categories-api.js';
 import { onCategoryClick } from './js/categories.js';
 import { onSearchClick } from './js/exercises.js';
+
 import './js/init-filters.js';
 import './js/group-list-listener.js';
