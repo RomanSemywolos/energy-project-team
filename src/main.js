5a49e12337bf0c86e958d8f7af7df3d44ca40024
Darina Khalina <darina.khalina@namecheap.com>
2024-07-04 19:39:47 +0300

    clean up

--- src/main.js
+++ src/main.js
@@ -5,5 +5,4 @@ import { onSearchClick } from './js/exercises.js';
 import './js/init-filters.js';
 import './js/search-clear-btn-listener.js';
 import './js/group-list-listener.js';
-import './js/filters-tabs-listener.js';
 import './js/search-form-listener.js';
