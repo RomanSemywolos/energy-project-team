488feee0e1a445561c731faa0f74e0beaba5b0a5
kryvosheyin <alex.kryvosheyin@gmail.com>
2024-07-05 15:32:56 +0100

    daily quote saves to local storage

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { getQuote } from './js/daily-quote.js';
+import { displayQuote } from './js/daily-quote.js';
 import { getCategories } from './js/api-service/categories-api.js';
 import { onCategoryClick } from './js/categories.js';
 import { onSearchClick } from './js/exercises.js';
