1324cc95a2ef2b8eb8dc1b7807ec37db7db3811f
Vlad Mazur <vladopermoz@gmail.com>
2024-07-06 15:32:21 +0300

    Remove console logs

--- src/js/api-service/exercices-api.js
+++ src/js/api-service/exercices-api.js
@@ -11,11 +11,7 @@ const getExercises = async (category, bodyPart, page = 1, limit = 10) => {
     page,
   });
 
-  console.log(`Sending API request: ${BASE_URL}${EXERCISES}?${params}`);
-
   const response = await axios.get(`${EXERCISES}?${params}`);
-  
-  console.log('API response:', response.data);
 
   const searchTerm = document.querySelector('.exercises_search-input').value.toLowerCase();
   const filteredResults = response.data.results.filter(exercise => 
