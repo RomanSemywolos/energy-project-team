7cd3a640ecb1c7434de4144a46283dc0162b2ca3
Denys Finchenko <dfinchenko@gmail.com>
2024-07-07 22:47:50 +0200

    menu

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,12 +1,14 @@
 function burgerButton() {
     const burger = document.querySelector('.burger');
-    const mobileMenu = document.querySelector('.nav-bar-mobile');
+    const mobileMenu = document.querySelector('.nav-bar-mobile-container');
     const body = document.querySelector('body');
   
     burger.addEventListener('click', function changeBurger() {
       burger.classList.toggle('change');
       mobileMenu.classList.toggle('is-hidden');
       body.classList.toggle('overlow');
+      mobileMenu.classList.toggle('nav-bar-mobile-overlay');
+      mobileMenu.classList.toggle('is-visible');
     });
   }
   
