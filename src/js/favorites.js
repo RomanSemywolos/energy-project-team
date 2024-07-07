8b98890456e475ec747bea5b4597a72ef02f42b6
Anna Samoilova <samoilova0299@gmail.com>
2024-07-07 13:51:28 +0300

    add favorites storage handler

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,15 +1,47 @@
-import { markupText } from './favorites-markup-text';
-import { markupList } from './favorites-markup-list';
-// import { listOfFavorites } from '../';
+import { createExercisesMarkup } from './render-exercises-list.js';
 
-export function addContent() {
-//   listOfFavorites();
+import {
+  getFavoritesData,
+  removeExerciseFromFavorites,
+} from './favorites-storage-handler.js';
 
-  const favorites = JSON.parse(localStorage.getItem('favorites'));
+let localData = [];
+const blankPageTextRef = document.querySelector('.favorite-text');
+const favoritesListRef = document.querySelector('.favourites_list');
 
-  if (!favorites?.length) {
-    markupText();
-    return;
-  }
-  markupList(favorites);
+// function onExerciseRemoveClick(e) {
+//   const target = e.target.closest('.exercise-remove-btn');
+//   if (!target) return;
+//   const exerciseID = target.getAttribute('data-exercise-id');
+//   if (exerciseID) {
+//     if (removeExerciseFromFavorites(exerciseID)) {
+//       handleFavoriteItems(getFavoritesData(), true);
+//     }
+//   }
+// }
+
+function itemHandler(arr) {
+  favoritesListRef.innerHTML = createExercisesMarkup(arr, true);
 }
+
+export function handleFavoriteItems(data = localData) {
+  if (!data.length) {
+    blankPageTextRef.classList.remove('visually-hidden');
+    favoritesListRef.innerHTML = '';
+    favoritesListRef.classList.add('hidden');
+    return;
+  } else {
+    blankPageTextRef.classList.add('visually-hidden');
+    favoritesListRef.classList.remove('hidden');
+  }
+  itemHandler(data);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.location.pathname.includes('favorites.html')) {
+    localData = getFavoritesData();
+    // Initial run
+    handleFavoriteItems(localData);
+    favoritesListRef.addEventListener('click', onExerciseRemoveClick);
+  }
+});
--- src/main.js
+++ src/main.js
@@ -8,3 +8,5 @@ import './js/header.js';
 import './js/burger.js';
 import './js/group-list-listener.js';
 import './js/modal-exercises.js';
+import './js/favorites.js';
+import './js/favorites-storage-handler.js';
