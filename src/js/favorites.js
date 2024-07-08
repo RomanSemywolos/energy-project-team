edde6ce4cc320349deba1d6d7757e4c49357ac49
Vlad Mazur <vladopermoz@gmail.com>
2024-07-08 14:05:49 +0300

    add listener in favorites to start btn

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,4 +1,12 @@
 import { createExercisesMarkup } from './render-exercises-list.js';
+import { getExerciseById } from './api-service/exercices-api';
+import {
+  createMarkup,
+  updateModal,
+  openModalExercises,
+  toggleBtn,
+  closeModalExercises,
+} from './modal-exercises';
 
 import {
   getFavoritesData,
@@ -8,6 +16,30 @@ import {
 let localData = [];
 const blankPageTextRef = document.querySelector('.favorite-text');
 const favoritesListRef = document.querySelector('.favourites_list');
+const startFavouritesExercise = document.querySelector('.exercise-card-header-btn');
+
+async function openExerciseModal(exerciseId) {
+  try {
+    const exerciseData = await getExerciseById(exerciseId);
+    window.currentExerciseData = exerciseData;
+
+    const markup = createMarkup(exerciseData);
+    updateModal(markup);
+    openModalExercises();
+
+    const btnModalFavorites = document.querySelector(
+      '.modal-exercises-btn-favorites'
+    );
+    btnModalFavorites.setAttribute('data-id', exerciseData._id);
+    btnModalFavorites.addEventListener('click', toggleBtn);
+    const btnModalClose = document.querySelector(
+      '.modal-exercises-btn-close'
+    );
+    btnModalClose.addEventListener('click', closeModalExercises);
+  } catch (error) {
+    console.error('ERROR:', error);
+  }
+}
 
 function onExerciseRemoveClick(e) {
   const target = e.target.closest('.exercise-remove-btn');
@@ -43,5 +75,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // Initial run
     handleFavoriteItems(localData);
     favoritesListRef.addEventListener('click', onExerciseRemoveClick);
+
+    // Add event listener for startFavouritesExercise
+    favoritesListRef.addEventListener('click', (event) => {
+      const startButton = event.target.closest('.exercise-card-header-btn');
+      if (startButton) {
+        const exerciseId = startButton.getAttribute('data-button-id');
+        openExerciseModal(exerciseId);
+      }
+    });
   }
 });
+
+
